fe364bd2b594ef55cea6c3c1830fa1515552fe3a
Unknown <roxydao97@gmail.com>
2017-03-17 11:47:46 +0200

    the new add customer updated

--- add_customers.php
+++ add_customers.php
@@ -1,6 +1,6 @@
 <?php include "menu.php"; ?>
 <h2>Add Customer</h2>
-<form class="" action="add_customers.php" method="post">
+<form class="" action="customers.php" method="post">
   <label for="fname">Firstname</label> <br/>
   <input type="text" name="fn" id="fn" value="" required=""><br>
 
@@ -13,18 +13,5 @@
   <input type="submit" name="btnAdd" value="Add">
 
 </form>
-<?php
-include 'connection.php';
-$btn = $_POST['btnAdd'];
-if (isset($btn)) {
-  $add=$db->prepare("INSERT INTO customers(firstname, lastname, streetaddress) VALUES(:fn,:ln,:ad)"); //name
-  $add->bindParam(':fn', $fn);
-  $add->bindParam(':ln', $ln);
-  $add->bindParam(':ad', $ad);
-  $fn = $_POST['fn'];
-  $ln = $_POST['ln'];
-  $ad = $_POST['ad'];
-  $add->execute();
-}
- ?>
+
 <?php include "footer.php"; ?>

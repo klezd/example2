eaddc97dccbbefc28a72be24814428c50efe0b46
Unknown <roxydao97@gmail.com>
2017-03-20 10:40:19 +0200

    Delete customers page added

--- customers.php
+++ customers.php
@@ -23,6 +23,11 @@ if (isset($_POST['btnEdit'])) {
   $id = $_POST['id'];
   $add->execute();
 }
+if (isset($_POST['btnDelete'])) {
+  $deleteSQL = "DELETE FROM customers WHERE id_customers=".$_POST['id'];
+  $db->query($deleteSQL);
+}
+
 
  ?>
 <?php include "menu.php"; ?>
@@ -43,7 +48,8 @@ $customers_data = $db->query($myquery);
 // print_r($customers_data);
 foreach ($customers_data as $row) {
   echo '<tr><td>'.$row['firstname'].'</td><td>'.$row['lastname'].'</td><td>'.$row['streetaddress'].'</td>';
-  echo '<td><a href="update_customer.php?id='.$row['id_customers'].'"><button>Update </button></a></td></tr>';
+  echo '<td><a href="update_customer.php?id='.$row['id_customers'].'"><button>Update </button></a></td>';
+  echo '<td><a href="delete_customer.php?id='.$row['id_customers'].'"><button>Delete </button></a></td></tr>';
 }
  ?>
  </table>
--- delete_customer.php
+++ delete_customer.php
@@ -1,5 +1,5 @@
 <?php include "menu.php"; ?>
-<h2>Edit customer</h2>
+<h2>Delete customer</h2>
 <?php
 include "connection.php";
 $sql_add = "SELECT * FROM customers WHERE id_customers=".$_GET['id'];
@@ -12,17 +12,19 @@ $ad = $theArray['streetaddress'];
 
 <form class="" action="customers.php" method="post">
   <label for="fn">Firstname</label> <br/>
-  <input type="text" name="fn" id="fn" value="<?php echo $fn; ?>" required=""><br>
+  <input type="text" name="fn" id="fn" value="<?php echo $fn; ?>" disabled=""><br>
 
   <label for="ln">Lastname</label> <br/>
-  <input type="text" name="ln" id="ln" value="<?php echo $ln; ?>" required=""><br>
+  <input type="text" name="ln" id="ln" value="<?php echo $ln; ?>" disabled=""><br>
 
   <label for="ad">Street address</label> <br/>
-  <input type="text" name="ad" id="ad" value="<?php echo $ad; ?>" required=""><br>
+  <input type="text" name="ad" id="ad" value="<?php echo $ad; ?>" disabled=""><br>
 
   <input type="hidden" name="id" value="<?php echo $_GET['id']; ?>"> <br/>
-  <input type="submit" name="btnEdit" value="Update"> <br/>
 
+  <label>Do you really want to delete this customer?</label> <br/>
+  <input type="submit" name="btnDelete" value="Delete"> &nbsp;
+  <a href="customers.php"> <button>Cancel</button> </a>
 </form>
 
 <?php include "footer.php"; ?>

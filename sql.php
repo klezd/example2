28da7b7ae2420f0637b6493dd5115d98057bf0db
Unknown <roxydao97@gmail.com>
2017-03-20 11:53:26 +0200

    sql updated

--- sql.php
+++ sql.php
@@ -13,5 +13,17 @@
 
   INSERT INTO customers(firstname, lastname) VALUE('Ann', 'Smith');
   INSERT INTO customers(firstname, lastname) VALUE('Tom', 'Jones');
+
+  CREATE TABLE cats(
+  id_cats SMALLINT PRIMARY KEY auto_increment,
+  name VARCHAR(30),
+  color VARCHAR(20),
+  id_customers SMALLINT,
+  FOREIGN KEY (id_customers) REFERENCES customers(id_customers)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE ) Engine =InnoDB;
+
+  INSERT INTO cats (name, color, id_customers) VALUES('Garfield', 'orange', 1);
+  INSERT INTO cats (name, color, id_customers) VALUES('Hello Kitty', 'pink', 2);
 </pre>
 <?php include "footer.php"; ?>

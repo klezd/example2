e20975ea5b87c45839f32a0c9891c8985d0e810d
Quynh Dao <roxydao97@gmail.com>
2017-03-23 09:28:47 +0200

    edit all customers page update

---
+++ edit_customers.php
@@ -0,0 +1,28 @@
+<?php include "menu.php"; ?>
+<h2>Edit customer</h2>
+<form class="" action="update_all_customers.php" method="post">
+  <table border="0">
+    <tr>
+      <th>First name</th>
+      <th>Last name</th>
+      <th>Street Address</th>
+    </tr>
+
+  <?php
+  include 'connection.php';
+  $myquery="SELECT firstname, lastname, streetaddress, id_customers FROM customers";
+  $customers_data = $db->query($myquery);
+  // print_r($customers_data);
+  foreach ($customers_data as $row) {
+    echo '<tr><td><input type="text" name="fn[]" value=" '.$row['firstname'].'"/></td>';
+    echo '<td><input type="text" name="ln[]" value=" '.$row['lastname'].'"/></td>';
+    echo '<td><input type="text" name="ad[]" value=" '.$row['streetaddress'].' "/></td>';
+    echo '<input type="hidden" name="id[]" value="'.$row['id_customers'].'"/>';
+    echo '</tr>';
+  }
+   ?>
+   </table>
+   <input type="submit" name="btnUpdateAll" value="Update">
+</form>
+
+<?php include "footer.php"; ?>
--- menu.php
+++ menu.php
@@ -17,6 +17,7 @@
                 <li> <a href="customers.php">Customers</a></li>
                 <li> <a href="add_customers.php">Add Customer</a></li>
                 <li> <a href="cats.php">Cats</a></li>
+                <li> <a href="edit_customers.php">Edit Customer</a></li>
             </ul>
         </div>
         <div id="content">
